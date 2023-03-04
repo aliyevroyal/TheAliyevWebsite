9526758ac33722694223e5791d564ee890a3eceb
aliyevroyal <royal.alyv@gmail.com>
2023-03-04 08:09:09 +0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,55 @@
+var mobileGWOID = document.getElementById("mobileGWOID");
+mobileGWOID.addEventListener("click", function () {
+  window.open(
+    "https://aliyevroyal.github.io/Blogs/GWO/WebPageFiles/WebPage.html"
+  );
+});
+
+var desktopGWOID = document.getElementById("desktopGWOID");
+desktopGWOID.addEventListener("click", function () {
+  window.open(
+    "https://aliyevroyal.github.io/Blogs/GWO/WebPageFiles/WebPage.html"
+  );
+});
+
+var mobileQLID = document.getElementById("mobileQLID");
+mobileQLID.addEventListener("click", function () {
+  window.open(
+    "https://aliyevroyal.github.io/Blogs/QLearning/WebPageFiles/WebPage.html"
+  );
+});
+
+var desktopQLID = document.getElementById("desktopQLID");
+desktopQLID.addEventListener("click", function () {
+  window.open(
+    "https://aliyevroyal.github.io/Blogs/QLearning/WebPageFiles/WebPage.html"
+  );
+});
+
+var mobileANNID = document.getElementById("mobileANNID");
+mobileANNID.addEventListener("click", function () {
+  window.open(
+    "https://aliyevroyal.github.io/Blogs/ANN/WebPageFiles/WebPage.html"
+  );
+});
+
+var desktopANNID = document.getElementById("desktopANNID");
+desktopANNID.addEventListener("click", function () {
+  window.open(
+    "https://aliyevroyal.github.io/Blogs/ANN/WebPageFiles/WebPage.html"
+  );
+});
+
+var mobileAdaptedRRTID = document.getElementById("mobileAdaptedRRTID");
+mobileAdaptedRRTID.addEventListener("click", function () {
+  window.open(
+    "https://aliyevroyal.github.io/Blogs/GWORRTPathPlanning/WebPageFiles/WebPage.html"
+  );
+});
+
+var desktopAdaptedRRTID = document.getElementById("desktopAdaptedRRTID");
+desktopAdaptedRRTID.addEventListener("click", function () {
+  window.open(
+    "https://aliyevroyal.github.io/Blogs/GWORRTPathPlanning/WebPageFiles/WebPage.html"
+  );
+});
